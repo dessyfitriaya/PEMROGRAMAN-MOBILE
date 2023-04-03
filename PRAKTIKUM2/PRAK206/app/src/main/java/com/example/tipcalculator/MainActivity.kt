9fb29bc21efaf9156687434493af5d77e19f99e2
Dessy Fitriaya Anwari <103984794+dessyfitriaya@users.noreply.github.com>
2023-04-03 23:11:22 +0800

    Add files via upload

---
+++ PRAKTIKUM2/PRAK206/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.tipcalculator
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.lang.Math.ceil
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    /*The lateinit keyword is something new.
+     It's a promise that your code will initialize the variable
+     before using it. If you don't, your app will crash.*/
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener {
+                view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
+    }
+
+    private fun calculateTip() {
+
+        //convert to string since its editable
+
+        var stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
